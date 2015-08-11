866f99b66429945633d974fb813e6c6a9a9ece73
bright.jiang <bright.jiang@infotmic.com.cn>
2015-08-11 18:43:59 +0800

    add line paint

--- handwritecore.cpp
+++ handwritecore.cpp
@@ -6,6 +6,7 @@ HandWriteCore::HandWriteCore(QWidget *parent)
     this->timer = new QTimer();
     this->timer->setInterval(this->timeout);
     this->timer->setSingleShot(true);
+    lineList.clear();
 }
 
 HandWriteCore::~HandWriteCore()
@@ -13,17 +14,57 @@ HandWriteCore::~HandWriteCore()
 
 }
 void HandWriteCore::mouseMoveEvent(QMouseEvent *event){
-   long long point_time =  QDateTime::currentDateTime().toMSecsSinceEpoch() - first_point_time ;
-    qDebug()<<"move ->"<<event->x() <<event->y() << point_time;
+   long long pointTime =  QDateTime::currentDateTime().toMSecsSinceEpoch() - this->firstPointTime ;
+
+    Point p ;
+    p.x  = event->x();
+    p.y = event->y();
+    p.timestamp = pointTime;
+
+//    this->lastPoint = p;
+//    this->curPoint = p;
+    Line  line ;
+    line.start = this->lastPoint;
+    line.end =  p ;
+    this->lineList.append(line);
+    this->lastPoint =  p ;
+    this->curPoint = p;
+
+
+    qDebug()<<"move ->"<<p.x <<p.y << p.timestamp;
 }
 void HandWriteCore::mousePressEvent(QMouseEvent *event){
-    this->first_point_time =  QDateTime::currentDateTime().toMSecsSinceEpoch();
-    qDebug()<<"press->"<<event->x() <<event->y() << 0;
+    this->firstPointTime =  QDateTime::currentDateTime().toMSecsSinceEpoch();
+
+    Point p ;
+    p.x  = event->x();
+    p.y = event->y();
+    p.timestamp = 0;
+
+    this->lastPoint = p;
+    this->curPoint = p;
+
+    qDebug()<<"press->"<<p.x <<p.y << p.timestamp;
+    this->repaint();
 }
 void HandWriteCore::mouseReleaseEvent(QMouseEvent *event){
 
     qDebug()<<"release->"<<event->x() <<event->y() ;
+    qDebug()<<this->lineList.size();
+    this->update();
 }
-
+void HandWriteCore::HandWriteCore::paintEvent ( QPaintEvent * event) {
+    QPainter painter(this);
+    QPen pen;
+    pen.setColor(QColor(255,0,0));  //设置画笔为红色
+    pen.setWidth(5);
+    painter.setPen(pen);  //选择画笔
+    Line line ;
+    foreach(line,this->lineList){
+//        qDebug()<<line.start.x<<line.start.y<<line.end.x<<line.end.y;
+        painter.drawLine(line.start.x,line.start.y,line.end.x,line.end.y); //用该红色画笔画一条线，起点(0,0)，终点(100,100)
+    }
+    painter.end(); //结束绘制。绘制时使用的任何资源都被释放。虽然有时不需要调用end()，析构函数将会执行它
+}
 
 
--- handwritecore.h
+++ handwritecore.h
@@ -6,24 +6,37 @@
 #include <QDebug>
 #include <QDateTime>
 #include <QTimer>
+#include <QList>
+#include <QPainter>
+#include <QPaintEvent>
 typedef  struct {
     int x ;
     int y;
     long long timestamp;
 } Point;
+typedef struct {
+    Point start ;
+    Point end;
+}Line;
 class HandWriteCore : public QWidget
 {
     Q_OBJECT
 private :
-    long long first_point_time ;
+    long long firstPointTime ;
     int timeout = 0;
     QTimer *timer ;
+    Point lastPoint ;
+    Point curPoint;
+   QList<Line> lineList;
 public:
     HandWriteCore(QWidget *parent = 0);
     ~HandWriteCore();
+
+protected:
     void mouseMoveEvent(QMouseEvent *event);
     void mousePressEvent(QMouseEvent *event);
     void mouseReleaseEvent(QMouseEvent *event);
+    void paintEvent ( QPaintEvent *event ) ;
 };
 
 #endif // HandWriteCore_H

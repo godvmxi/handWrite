71719e30b1849bc8541dbe17684d9e3e3c88cf67
bright.jiang <bright.jiang@infotmic.com.cn>
2015-08-14 17:58:42 +0800

    save

--- core/recongnizer_server.py
+++ core/recongnizer_server.py
@@ -2,20 +2,34 @@
 from tegaki.recognizer import Recognizer
 from ConfigParser import SafeConfigParser, NoSectionError, NoOptionError
 import socket, traceback
+import sys
+import pprint
 
 host = ''
 port = 10001
-
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((host, port))
-
-while 1:
-    try:
-        message, address = server.recvfrom(10001)
-        print "Got data from", address, ": ", message
-        server.sendto(message, address)
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        traceback.print_exc()
+def create_udp_server(port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('', port))
+    return server
+def select_recongnizer_models(name):
+    pass
+def get_all_availabke_models():
+    all_models = Recognizer.get_all_available_models()
+    
+    pprint.pprint(all_models)
+    for r_name, model_name, meta in all_models :
+    
+if __name__ == "__main__": 
+    print()
+    get_all_availabke_models()
+    sys.exit()
+    while 1:
+        try:
+            message, address = server.recvfrom(10001)
+            print "Got data from", address, ": ", message
+            server.sendto(message, address)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            traceback.print_exc()
